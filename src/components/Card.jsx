39b670996e7c677fb8e5a4167a46d351e760ea1b
charlesoller <charlesrello@gmail.com>
2023-10-12 18:02:11 -0400

    Fixed paragraph bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import cloudIcon from '../assets/cloud-icon.svg';
 
 export default function Card({ icon, title, children, color }){
+
+    // console.log(children)
     return (
         <div className={`card card--${color}`}>
             <div className="card__content">
                 <h1 className="card__title">{title}</h1>
-                <p className="card__body">{children}</p>
+                <div className="card__body">{children}</div>
             </div>
             <div className="card__icon__bg">
                 <img className="card__icon" src={cloudIcon} />
