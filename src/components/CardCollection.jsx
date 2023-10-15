44a8aef2297c748040c011cfc0cf56e3b8c303a1
charlesoller <charlesrello@gmail.com>
2023-10-15 17:57:17 -0400

    before messing with framer motion

--- src/components/CardCollection.jsx
+++ src/components/CardCollection.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Card from './Card'
 import { FaApple, FaBtc, FaReact } from "react-icons/fa";
+import { motion } from "framer-motion"
 
 export default function CardCollection(){
-    //this should also accept an icon prop below
+    const [visible, setVisible] = useState(false);
+    const container = {
+        invisible: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.5
+            }}
+    }
+    const item = {
+        invisible: { opacity: 0 },
+        visible: { opacity: 1 }
+    }
+
     return (
         <div className='card-grid'>
             <h1 className="card-collection__title section__header">Card Component</h1>
-            <Card title="Easy Deployment">
-                <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
-            </Card>
-
-            <Card title="So Modular!" icon={<FaApple size={'1.5rem'} color='lightgray'/>} color='darkgray' width='80%' darkMode='dark'>
-                <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
-            </Card>
-
-            <Card color='#E66955' icon={<FaBtc color='white'/>} width='55%'>
-                <p>Doesn't even need a title!</p>
-            </Card>
-
-            <Card title="Or children!" icon={<FaReact size={'1.5rem'} color='lightgray'/>} color='#1C2294' darkMode='dark'/>
+            <button onClick={()=> setVisible(visible => !visible)}>CLICK</button>
+            <motion.ol
+                animate={visible ? "visible" : "invisible"}
+                variants={container}
+            >
+                <motion.li variants={item}>
+                    <Card title="Easy Deployment">
+                        <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+                    </Card>
+                </motion.li>
+                <motion.li variants={item}>
+                    <Card title="So Modular!" icon={<FaApple size={'1.5rem'} color='lightgray'/>} color='darkgray' width='80%' darkMode='dark'>
+                        <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+                    </Card>
+                </motion.li>
+                <motion.li variants={item}>
+                    <Card color='#E66955' icon={<FaBtc color='white'/>} width='55%'>
+                        <p>Doesn't even need a title!</p>
+                    </Card>
+                </motion.li>
+                <motion.li variants={item}>
+                    <Card title="Or children!" icon={<FaReact size={'1.5rem'} color='lightgray'/>} color='#1C2294' darkMode='dark'/>
+                </motion.li>
+            </motion.ol>
         </div>
     )
 }
